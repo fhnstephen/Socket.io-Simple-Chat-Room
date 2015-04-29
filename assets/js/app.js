d8151e4be3566506cef1352a1c03895712b17144
fhnstephen <fhnstephen@gmail.com>
2015-04-29 12:09:16 +0800

    Add JWT for authorize.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,36 @@
 angular.module('MyApp', [
   'btford.socket-io'
-]).controller('MasterCtrl', function($scope, mySocket) {
-  var socket = mySocket;
-  $scope.sendMessage = function(mes) {
-    socket.emit('chat message', mes);
-  };
+]).controller('MasterCtrl', function($scope, mySocket, $http) {
+  var token = null;
   $scope.messages = [];
   var count = 0;
-  socket.on('chat message', function(mes) {
-    $scope.messages.push({id: ++count, data: mes});
+
+  $scope.connect = function (name) {
+    var data = {
+      name: name
+    };
+    $http.post('/login', data).then(function (result) {
+      $scope.token = result.data;
+      console.log($scope.token);
+      $scope.socket = io('', {'query': 'token=' + $scope.token.token});
+      $scope.socket.on('connect', function () {
+        console.log('Connected to server');
+        $scope.$emit('connect');
+      }).on('error', function (mes) {
+        console.log('Error:', mes);
+      }).on('chat message', function(mes) {
+        $scope.messages.push({name: mes.name, data: mes.mes});
+        $scope.$apply();
+      });
+    }, function (result) {
+      console.log(result);
+    });
+  };
+  $scope.sendMessage = function(mes) {
+    $scope.socket.emit('chat message', mes);
+  };
+  $scope.$on('connect', function() {
+    $scope.connected = true;
     $scope.$apply();
   });
 });
--- server.js
+++ server.js
@@ -4,6 +4,35 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var jwt = require('jsonwebtoken');
+var socketioJwt = require('socketio-jwt');
+var bodyParser = require('body-parser');
+
+
+var jwtSecret = "123456789";
+
+var users = 0;
+
+io.use(socketioJwt.authorize({
+  secret: jwtSecret,
+  handshake: true
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+app.post('/login', function (req, res) {
+  var profile = {
+    name: req.body.name,
+    id: users++,
+  };
+  var token = jwt.sign(profile, jwtSecret, { expiresInMinutes: 60*24});
+  res.json({token: token});
+});
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -18,12 +47,13 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  socket.name = socket.decoded_token.name;
+  console.log('Hello', socket.name);
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    console.log('Bye',  socket.name);
   });
   socket.on('chat message', function(mes) {
-    io.emit('chat message', mes);
+    io.emit('chat message', {name: socket.name, mes: mes});
   });
 });
 
